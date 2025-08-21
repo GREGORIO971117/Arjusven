31c33c7d0bf106f5c7c5c291c32b5937e2b385db
Gregorio <gregorio.galindo9711@hotmail.com>
2025-08-21 11:13:59 -0600

    Se agrego el filtro de ticket cerrados y abierto

--- src/Components/ticketTemplate/TicketPage.jsx
+++ src/Components/ticketTemplate/TicketPage.jsx
@@ -1,43 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import TicketTemplate from './TicketTemplate';
 import { useNavigate } from 'react-router-dom';
-import ticketsData from '../../assets/ticketsData.json';
+import ticketsData from '../../assets/ticketsData.json'; 
 
 function TicketPage() {
-  const [ticketData, setTicketData] = useState(null);
+  const [ticketsList, setTicketsList] = useState([]);
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate(); // Inicializa el hook de navegación
+  const [currentStatusFilter, setCurrentStatusFilter] = useState('all'); 
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Simula una llamada a la API con un retraso
     setTimeout(() => {
-      setTicketData(ticketsData);
+      setTicketsList(ticketsData);
       setLoading(false);
     }, 1500);
   }, []);
 
   const handleAddTicket = () => {
-    // Redirige al usuario a la ruta del formulario de Service Request
     navigate('/tickets/add');
   };
 
+  const handleFilterChange = (status) => {
+    setCurrentStatusFilter(status);
+  };
+
+  const filteredTickets = ticketsList.filter(ticket => {
+    if (currentStatusFilter === 'all') {
+      return true;
+    }
+    return ticket.serviceRequest.currentStatus === currentStatusFilter;
+  });
+
   if (loading) {
-    return <div>Cargando plantilla de ticket...</div>;
+    return <div>Cargando plantilla de tickets...</div>;
   }
 
   return (
     <div>
+      <div className="button-group">
+        <button 
+          onClick={() => handleFilterChange('abierta')} 
+          className={`filter-button ${currentStatusFilter === 'abierta' ? 'active' : ''}`}
+        >
+          Tickets Abiertos
+        </button>
+        <button 
+          onClick={() => handleFilterChange('cerrada')} 
+          className={`filter-button ${currentStatusFilter === 'cerrada' ? 'active' : ''}`}
+        >
+          Tickets Cerrados
+        </button>
+        <button 
+          onClick={() => handleFilterChange('all')} 
+          className={`filter-button ${currentStatusFilter === 'all' ? 'active' : ''}`}
+        >
+          Todos los Tickets
+        </button>
+      </div>
+
       <button onClick={handleAddTicket} className="add-button">
         Agregar
       </button>
-      <button className="add-button">
-        Tickets Pendientes
-      </button>
-      <button className="add-button">
-        Tickets Cerrados
-      </button>
 
-     <TicketTemplate data={ticketData} />
+      {/* Se renderiza la lista filtrada de tickets */}
+      {filteredTickets.map((ticket, index) => (
+        <TicketTemplate key={index} data={ticket} />
+      ))}
     </div>
   );
 }
