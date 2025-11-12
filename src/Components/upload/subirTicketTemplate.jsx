7a14c40bca80895977bc142b7f6cd74d3eba5fda
Gregorio <gregorio.galindo9711@hotmail.com>
2025-11-12 12:42:32 -0600

    Se implementa la funcionalidad de estaciones

--- src/Components/upload/subirTicketTemplate.jsx
+++ src/Components/upload/subirTicketTemplate.jsx
@@ -51,6 +51,7 @@ export default function SubirTicketTemplate() { // Desestructuramos datosEstatic
    
         const payload = {
                 "administrador": {
+
                 },
                 "servicios": {
                     "motivoDeServicio": motivoServicio.trim(),
@@ -59,6 +60,7 @@ export default function SubirTicketTemplate() { // Desestructuramos datosEstatic
                     "idMerchant": idMerchant.trim(),
                 },
                 "adicionales":{
+                    
                 }
             };
         
